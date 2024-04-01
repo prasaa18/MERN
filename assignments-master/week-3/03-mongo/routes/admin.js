72b408f54e46f27a8b783cf45fd1ec0559dd3da2
prasa18 <prasadsnaik18@gmail.com>
2024-04-01 17:37:11 +0530

    coursees apis  using express in week 4 assignmnets

--- assignments-master/week-3/03-mongo/routes/admin.js
+++ assignments-master/week-3/03-mongo/routes/admin.js
@@ -1,18 +1,68 @@
 const { Router } = require("express");
 const adminMiddleware = require("../middleware/admin");
+const { Admin, Course } = require("../db");
 const router = Router();
 
 // Admin Routes
-router.post('/signup', (req, res) => {
+router.post('/signup', async(req, res) => {
     // Implement admin signup logic
+
+    const username=req.body.username;
+    const password=req.body.password;
+
+    //check username alraedy exists
+   await Admin.create({
+        username:username,
+        password:password
+    })
+    res.json({
+        message:"Admin Created Successfully "
+    })
+    // .then(function(){
+    //     res.json({
+    //         message:'Admin created Successfully'
+    //     })
+    // })
+    // .catch(function(){
+          
+    // })
+  
 });
 
-router.post('/courses', adminMiddleware, (req, res) => {
+router.post('/courses', adminMiddleware, async(req, res) => {
     // Implement course creation logic
+    const title=req.body.title;
+    const description=req.body.description;
+    const imageLink=req.body.imageLink;
+    const price=req.body.price;
+    //zod
+  const newcourse= await Course.create({   //key and value are same 
+        title,
+        description,
+        imageLink,
+        price
+    })
+    console.log(newcourse);
+    res.json({
+        message:'course created successfully', courseId:newcourse._id
+    })
+
 });
 
-router.get('/courses', adminMiddleware, (req, res) => {
+router.get('/courses', adminMiddleware, async(req, res) => {
     // Implement fetching all courses logic
+
+    //  Course.find({})   //all courses
+    //  .then(function(response){
+    //     res.json({
+    //         courses:response
+    //     })
+    //  })
+    const response = await Course.find({})
+   res.json({
+    courses:response
+   })
+   
 });
 
 module.exports = router;

c4aa86d356edbe91e1d95a7a5ccda972421c646a
prasadnaik1-admin <prasadsnaik18@gmail.com>
2024-05-13 17:50:30 +0530

    useCallback useState useEffect example

--- Foundation-class/class-6/Hooks/Hooks/src/App.jsx
+++ Foundation-class/class-6/Hooks/Hooks/src/App.jsx
@@ -1,4 +1,4 @@
-import { useEffect, useState } from 'react'
+import { useEffect, useMemo, useState } from 'react'
 
 import axios from "axios"
 import './App.css'
@@ -18,6 +18,8 @@ function App() {
 
   return <div>
 
+
+    
     <button onClick={function(){
       setSelectedId(1);
     }}> 1</button>
@@ -25,8 +27,9 @@ function App() {
 <button onClick={function(){
       setSelectedId(2);
     }}> 2</button>
-
+      
      <Todo id={selectedId}/>
+    
   </div>
   
 
@@ -45,18 +48,18 @@ function App() {
 //     <h1>{title}</h1>
 //     <h2>{description}</h2>
 //   </div>
-// }
+// } d
 
 
 function Todo({id}){
   const [todo, setTodos]=useState({});
 
-   useEffect(()=>{
-    axios.get(`https://sum-server.100xdevs.com/todo?id=${id}`)
-    .then(response=>{
-      setTodos(response.data.todo);
-    })
-   },[id])
+    useEffect(()=>{
+      axios.get(`https://sum-server.100xdevs.com/todo?id=${id}`)
+      .then(response=>{
+        setTodos(response.data.todo);
+      })
+    },[id])
 
   return <div>
     {id}
@@ -65,6 +68,73 @@ function Todo({id}){
 </div>
 }
 
+function  Sum(){
+   const [counter, setCounter]=useState(0);
+   const [inputValue, setInputValue]=useState(1);
 
+
+   const[count1, setCount]=useState(0);
+
+  //  let count =0;
+  //  for (let i =1; i<=inputValue; i++){
+  //   count+=i;   }
+// usememo change on;y when input chages
+
+  let count = useMemo(()=>{
+    let finalCount =0;
+    for(let i=0; i<=inputValue; i++){
+      finalCount+=i;
+    }
+    return finalCount;
+  },[inputValue])
+
+// useeffect include extra  input  7]bnon[8on]
+  useEffect(()=>{
+    let finalCount=0;
+    for (let i=0; i<=inputValue; i++){
+      finalCount+=i;
+    }
+    setCount(finalCount);
+  }, [inputValue])
+
+
+
+   return <div>
+    <input onChange={function(e){setInputValue(e.target.value)}} placeholder='sum from 1 to n'></input>
+    <br />
+    sum from 1 to n {inputValue} is {count}
+    <br />
+    <button onClick={()=>{
+      setCounter(counter+1)
+    }}> counter {{ counter}}</button>
+   </div>
+}
+
+
+
+
+//custom hook
+
+function useTodos(){
+   const[todos, setTodos]=useState([]);
+  useEffect(()=>{
+    axios.get(`https://sum-server.100xdevs.com/todo?id=${id}`)
+    .then(response=>{
+      setTodos(response.data.todo);
+    })
+   },[])
+   return todos;
+
+}
+
+function App (){
+  const todo= useTodos();
+  return <>
+  
+  {{todo}}
+  
+  </>
+}
+ 
 
 export default App
